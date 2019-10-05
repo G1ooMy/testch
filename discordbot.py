fc1872f061d6b607477e47044c66393a5110ac7a
discord.py <56204626+G1ooMy@users.noreply.github.com>
2019-10-06 08:58:06 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -1,19 +1,39 @@
-from discord.ext import commands
-import os
-import traceback
+import discord
+import sys
 
-bot = commands.Bot(command_prefix='/')
-token = os.environ['DISCORD_BOT_TOKEN']
+# さいころの和を計算する用の関数
+from func import  diceroll
 
+TOKEN = 'NjMwMTY5MzUyNjQwOTIxNjQw.XZkZpA.3NWnsJ8MyF4V8hdcx61xspafI60'
 
-@bot.event
-async def on_command_error(ctx, error):
-    await ctx.send(str(error))
+client = discord.Client()
 
+@client.event
+async def on_ready():
+    print('--------------')
+    print('ログインしました')
+    print(client.user.name)
+    print(client.user.id)
+    print('--------------')
+    channel = client.get_channel('チャンネルID')
+    await channel.send('楽しいTRPGを始めましょう！')
 
-@bot.command()
-async def ping(ctx):
-    await ctx.send('pong')
+@client.event
+async def on_message(message):
+    if message.author.bot:
+        return
+    if message.content.startswith("!dice"):
+        # 入力された内容を受け取る
+        say = message.content 
 
+        # [!dice ]部分を消し、AdBのdで区切ってリスト化する
+        order = say.strip('!dice ')
+        cnt, mx = list(map(int, order.split('d'))) # さいころの個数と面数
+        dice = diceroll(cnt, mx) # 和を計算する関数(後述)
+        await message.channel.send(dice[cnt])
+        del dice[cnt]
 
-bot.run(token)
+        # さいころの目の総和の内訳を表示する
+        await message.channel.send(dice)
+
+client.run(NjMwMTY5MzUyNjQwOTIxNjQw.XZkZpA.3NWnsJ8MyF4V8hdcx61xspafI60)
